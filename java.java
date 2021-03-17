fd373b1520a06c2d999874e8c4c6bb8ebc103f3e
sinha-nischay <nischaysinha1@gmail.com>
2021-03-17 13:29:23 +0530

    for presentation

---
+++ java.java
@@ -0,0 +1,10 @@
+
+class java {
+    public static void main(String[] args) {
+        System.out.println("\nHello, World!\n");
+	System.out.println("\nthis is proving to be time consuming\n");
+	 System.out.println("\nhope to do my next taskes as quickly as possible\n");
+	 System.out.println("\ntill then..\n");
+	 System.out.println("\nHello, World!\n"); 
+    }
+}
